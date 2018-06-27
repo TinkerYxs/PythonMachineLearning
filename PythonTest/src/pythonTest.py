ca28d3265c670ea54dc90450396d21a859676065
Thinker <dashuai19910812@163.com>
2018-06-27 21:13:27 +0800

    pythonTest和一个maven项目

---
+++ PythonTest/src/pythonTest.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Function:
+【记录】使用Python的IDE：Eclipse+PyDev
+ 
+http://www.crifan.com/try_with_python_ide_eclipse_pydev
+ 
+Author:     Crifan Li
+Version:    2012-12-29
+Contact:    admin at crifan dot com
+"""
+  
+#import platform;
+
+print ("Hello, Python!");
+ 
+#print ("Current platform.uname() in Ecplise+PyDev=" ),platform.uname();
+
+#temp = input("please enter a number:\n");
+
+#number=int(temp);
+
+#if number == 8:
+#    print("ok!");
+#else:
+#    print("no");
+#print("--------------------------------------------------");
+
+import sys
+print("Python version:{}".format(sys.version))
+
+import pandas as pd
+print("pandas version:{}".format(pd.__version__))
+
+import matplotlib
+print("matplotlib version:{}".format(matplotlib.__version__))
+
+import numpy as np
+print ("Numpy version:{}".format(np.__version__))
+
+import scipy as sp
+print("Scipy version:{}".format(sp.__version__))
+
+import IPython
+print("IPython version:{}".format(IPython.__version__))
+
+import sklearn
+print("scikit-learn version:{}".format(sklearn.__version__))
+
+
---
+++ yxs1/src/main/java/Yuan/yxs1/App.java
@@ -0,0 +1,15 @@
+package Yuan.yxs1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        
+    }
+}
